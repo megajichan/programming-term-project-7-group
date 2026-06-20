4663f1f094aa822d28bf86202a1dafc98633997f
fjk54fw5zv-hue <fjk54fw5zv@privaterelay.appleid.com>
2026-06-20 11:22:10 +0900

    Update termproject.h

--- termproject.h
+++ termproject.h
@@ -1,18 +1,25 @@
-#pragma once // 헤더 파일 중복 포함 방지
+#ifndef TERMPROJECT_H
+#define TERMPROJECT_H
 #include <string>
-
-using namespace std;
+#include <QString>
 
 class termproject {
 private:
-    int rows;
-    int cols;
-    unsigned char* pixelValues;    // 원본 이미지 저장용 동적 할당 포인터
-    unsigned char* pixelProcessed; // 영상처리된 이미지 저장용 동적 할당 포인터
-
+    int cols = 0;
+    int rows = 0;
+    unsigned char* pixelValues = nullptr;
+    unsigned char* pixelProcessed = nullptr;
 public:
-    termproject(string fname);        // 생성자: 파일 읽기 및 메모리 할당
-    int imageProc(int Mode);        // 영상 처리 함수
-    int imageWrite(string fname);   // 파일 저장 함수
-    ~termproject(void);               // 소멸자: 메모리 반납
+    termproject(std::string fname);
+    termproject(QString qFname);
+    ~termproject();
+    int imageProc(int Mode);
+    int imageReset();
+    int imageWrite(std::string fname);
+    int imageWrite(QString qFname);
+    int getWidth() const { return cols; }
+    int getHeight() const { return rows; }
+    unsigned char* getProcessedData() const { return pixelProcessed; }
+    unsigned char* getOriginalData() const { return pixelValues; }
 };
+#endif // TERMPROJECT_H
